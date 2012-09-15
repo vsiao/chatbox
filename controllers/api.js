49f70cbbe727967a7e00724395777ce16baf213f
Peter Xiao <peterthexiao@gmail.com>
2012-09-15 14:22:24 -0400

    fixed get api

--- controllers/api.js
+++ controllers/api.js
@@ -7,19 +7,21 @@ module.exports = function(app, dropboxdb) {
     dropboxdb.userInfo(function(userInfo) {
       name = userInfo['name'];
       email = userInfo['email'];
+      console.log(req);
+      dropboxdb.insert(
+        chatsTable, 
+        {from: email, to: req.params.user, msg: req.params.msg},
+        function(){}  
+      );
     });
-    dropboxdb.insert(
-      chatsTable, 
-      {from: email, to: req.params.user, msg: req.body.msg},
-      function(){});
   });
 
   /* TODO: a get that doesn't get all */
   app.get('/api/get/:user', function(req, res) {
-    dropbox.find(
+    dropboxdb.find(
       chatsTable,
-      function(row) {row["from"] === req.params.user || row["to"] === req.params.user},
-      function(lines) {res.send(ok(lines))}
+      function(row) {return row["from"] === req.params.user || row["to"] === req.params.user},
+      function(lines) {res.send(lines)}
     );
   });
 
