34636b0f1b3007d8695da4c524ad1540a8797ef0
Peter Xiao <peterthexiao@gmail.com>
2012-09-15 13:26:10 -0400

    added api.js to app.js. fixed other issues.

--- controllers/misc.js
+++ controllers/misc.js
@@ -4,6 +4,7 @@ module.exports = function(app, dropboxdb) {
       if (error) {
         res.render('index', {msg: 'You\'re a fuck up.'});
       } else {
+        dropboxdb.create("chats", {}, function(){});
         res.render('index', {msg: 'You authenticated!!'});
       }
     });
